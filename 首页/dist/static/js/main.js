e7ad5f501acf9432cadce9139fc64e3043f41183
PUGE <100284685@qq.com>
2019-12-16 00:39:05 +0800

    完成首页H5

--- 首页/dist/static/js/main.js
+++ 首页/dist/static/js/main.js
@@ -1,1 +1,6 @@
-
+var scale = document.documentElement.clientWidth / document.documentElement.clientHeight
+if ((scale) < 1) {
+  document.body.classList.add('phone')
+} else {
+  document.body.classList.add('pc')
+}
